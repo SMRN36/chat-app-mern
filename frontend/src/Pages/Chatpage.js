cb98ce009a27f80e319c8d230733f8c38cf3d04a
simran bhandari <simranbhandari036@gmail.com>
2023-06-13 20:28:21 +0530

    Chat logic, search and notification added

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -4,11 +4,16 @@ import { useHistory } from "react-router";
 import Chatbox from "../components/Chatbox";
 import MyChats from "../components/MyChats";
 import SideDrawer from "../components/SideDrawer";
+import { Box } from "@chakra-ui/layout";
 
 const Chatpage = () => {
   const history = useHistory();
   const [user, setUser] = useState();
   const [selectedChat, setSelectedChat] = useState();
+  const [chats, setChats] = useState();
+  const [fetchAgain, setFetchAgain] = useState(false);
+
+
 
   useEffect(() => {
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
@@ -20,25 +25,35 @@ const Chatpage = () => {
 
   return (
     <div style={{ width: "100%" }}>
-      {user && <SideDrawer user={user} />}
-      <div
-        style={{
-          width: "100%",
-          height: "91.5vh",
-          display: "flex",
-          justifyContent: "space-between",
-          padding: 10,
-        }}
-      >
+    {user && (
+      <SideDrawer
+        user={user}
+        setSelectedChat={setSelectedChat}
+        setChats={setChats}
+        chats={chats}
+      />
+    )}
+    <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
         {user && (
           <MyChats
             user={user}
+            chats={chats}
+            setChats={setChats}
             selectedChat={selectedChat}
             setSelectedChat={setSelectedChat}
+            fetchAgain={fetchAgain}
           />
         )}
-        {user && <Chatbox selectedChat={selectedChat} user={user} />}
-      </div>
+        {user && (
+          <Chatbox
+            setSelectedChat={setSelectedChat}
+            selectedChat={selectedChat}
+            user={user}
+            fetchAgain={fetchAgain}
+            setFetchAgain={setFetchAgain}
+          />
+        )}
+        </Box>
     </div>
   );
 };
